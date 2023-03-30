bcf780bb119f9217f692cfad8d723ba8d7228b7c
Narendra Yudan <110644292+Narendrayudan@users.noreply.github.com>
2023-03-30 11:12:30 +0700

    Add files via upload

---
+++ run.java
@@ -0,0 +1,13 @@
+
+import static java.lang.Character.getName;
+
+public class run {
+   public static void main(String[] args) {
+      Modifier mPerson = new Modifier();
+      mPerson.getName = "Ydn";
+      
+       System.out.println("Person name: "+ mPerson.getName);
+   
+}
+
+}

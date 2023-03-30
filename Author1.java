3f80599f05bc743d0b6b667a9a58927bc657ad1f
Narendra Yudan <110644292+Narendrayudan@users.noreply.github.com>
2023-03-30 10:54:36 +0700

    Add files via upload

---
+++ Author1.java
@@ -0,0 +1,10 @@
+public class Author1 {
+    class Person{
+        public String name;
+        
+        public void changeName(String newName){
+            this.name=newName;
+        }
+    }
+    
+}
